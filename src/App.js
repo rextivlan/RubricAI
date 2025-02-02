8b8c8a41702b7efbe47b614e90645a2bf9936194
rextivlan <affan.ali368@gmail.com>
2025-02-02 16:18:08 -0500

    Init Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { Rubric } from './components/Rubrick';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          <Rubric />
+      </header>
+    </div>
+  );
+}
+
+export default App;
